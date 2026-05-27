ae8d4c7e6b5e148d6361f37150f2ac6803d89636
Laucarr <laura.carrard@crea-inseec.com>
2026-05-27 10:39:04 +0200

    CRUD and Form: Adopter

--- src/Controller/AdopterController.php
+++ src/Controller/AdopterController.php
@@ -2,17 +2,80 @@
 
 namespace App\Controller;
 
+use App\Entity\Adopter;
+use App\Form\AdopterType;
+use App\Repository\AdopterRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/adopter')]
 final class AdopterController extends AbstractController
 {
-    #[Route('/adopter', name: 'app_adopter')]
-    public function index(): Response
+    #[Route(name: 'app_adopter_index', methods: ['GET'])]
+    public function index(AdopterRepository $adopterRepository): Response
     {
         return $this->render('adopter/index.html.twig', [
-            'controller_name' => 'AdopterController',
+            'adopters' => $adopterRepository->findAll(),
         ]);
+    }
+
+    #[Route('/new', name: 'app_adopter_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $adopter = new Adopter();
+        $form = $this->createForm(AdopterType::class, $adopter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($adopter);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_adopter_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('adopter/new.html.twig', [
+            'adopter' => $adopter,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_adopter_show', methods: ['GET'])]
+    public function show(Adopter $adopter): Response
+    {
+        return $this->render('adopter/show.html.twig', [
+            'adopter' => $adopter,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_adopter_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Adopter $adopter, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(AdopterType::class, $adopter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_adopter_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('adopter/edit.html.twig', [
+            'adopter' => $adopter,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_adopter_delete', methods: ['POST'])]
+    public function delete(Request $request, Adopter $adopter, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$adopter->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($adopter);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_adopter_index', [], Response::HTTP_SEE_OTHER);
     }
 }
---
+++ src/Form/AdopterType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Adopter;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdopterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+                'required' => true
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+                'required' => true
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'required' => true
+            ])
+            ->add('phone', TelType::class, [
+                'label' => 'Téléphone',
+                'required' => false
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse',
+                'required' => true
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Adopter::class,
+        ]);
+    }
+}
